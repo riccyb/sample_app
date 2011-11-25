037ddf8d083dbe0010c0f7875aee575100cf8d7b
Riccardo <riccardo@UBUNTU-VBOX.(none)>
2011-11-25 02:12:04 +0100

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+#Return a title on per-page basis											#Documentation Comment
+	def title                                           #Definizione del metodo
+		base_title = "Ruby on Rails Tutorial Sample App"  #Assegnazione Variabile
+		if @title.nil?																		#Test Booleano su nil
+				base title																		#Implicit Return
+		else
+			"#{base_title} | #{@title}"											#String Interpolation
+		end
+	end
 end
